5ff7fe6bbde13f8fe3aebd4f32ea43b57c8af9c9
spartan500213 <parthpatilsunil@gmail.com>
2024-12-06 22:17:12 +0530

    added convAOLtoCOO, convAOLtoCSR

--- sparse_matrix/demo/conversion_functions.c
+++ sparse_matrix/demo/conversion_functions.c
@@ -1,13 +1,57 @@
 #include "head.h"
 
-void convAOLtoCOO() {
-
+void convAOLtoCOO(SparseMat *aol, SparseMat *coo) {
+    //in function variables
+    ulint i, coo_index = 0; AOLNode *temp;
+    //both matrices are already initialised
+    if(aol->nnz == 0) {
+        _flag = 5001;
+        return;
+    }
+    coo->nnz = aol->nnz;
+    coo->coo_mat->arr = (COONode *) malloc(sizeof(COONode) * coo->nnz);
+    if(coo->coo_mat->arr == NULL) {
+        _flag = 1002; return;
+    }
+    for(i = 0; i < aol->rows; i++) {
+        temp = aol->aol_mat->rows[i];
+        while(temp != NULL) {
+            coo->coo_mat->arr[coo_index].data = temp->data;
+            coo->coo_mat->arr[coo_index].col = temp->col;
+            coo->coo_mat->arr[coo_index].row = i;
+            coo_index++; 
+        }   
+    }
+    return;
 }
 
-void convAOLtoCSR() {
-    
-}
+void convAOLtoCSR(SparseMat *aol, SparseMat *csr) {
+    //in function variables
+    ulint row_count; ulint i; ulint csr_arr_index = 0; ulint csr_row_index = 0;
+    AOLNode *temp;
+    //both matrices are already initialised
+    if(aol->nnz == 0) {
+        _flag = 5001; return;
+    }
+    csr->nnz = aol->nnz;
+    csr->csr_mat->arr = (CSRNode *) malloc(sizeof(CSRNode) * csr->nnz);
 
+    for(i = 0; i < aol->rows; i++) {
+        row_count = 0;
+        temp = aol->aol_mat->rows[i];
+        while(temp != NULL) {
+            csr->csr_mat->arr[csr_arr_index].data = temp->data;
+            csr->csr_mat->arr[csr_arr_index].col = temp->col;
+            row_count++;
+            temp = temp->next;
+            csr_arr_index++;    
+        }
+        csr->csr_mat->row_entries[csr_row_index] = row_count;
+        csr_row_index++;
+    }
+    return;
+}
+/*
 void convCOOtoAOL() {
 
 }
@@ -23,3 +67,4 @@ void convCSRtoAOL() {
 void convCSRtoCOO() {
 
 }
+*/
